d41ce4797ab48f408b284fdba2aa4cde7bbf6565
SubaruSteve <mjfulcher58@gmail.com>
2020-06-13 22:18:11 +1200

    Add files via upload
    calculation of progress percentage is more efficient using floor division to avoid the need for type conversion.

--- progressbar_control.py
+++ progressbar_control.py
@@ -6,7 +6,8 @@ def setup_progressbar():
     bar = ProgressBar(maxval=100, widgets=[Bar('=', '[', ']'), ' ', Percentage()])
 
 def update_progressbar(iterator, denominator):
-    progress = int(iterator/denominator * 100)
+##    progress = int(iterator/denominator * 100)
+    progress = iterator * 100 // denominator
     bar.update(progress)
     stdout.flush()
     if progress == 100:
